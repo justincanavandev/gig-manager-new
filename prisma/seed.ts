799272aae746b4df3163e9d9aa100faa829d48bc
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-08 21:27:14 -0500

    added seed data for venues and musicians

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import { musicians, instruments, venues } from "./seedData";
 
-const seedInstruments = async () => {
-  const instruments = [
-    "Electric Guitar",
-    "Acoustic Guitar",
-    "Bass",
-    "Drums",
-    "Percussion",
-    "Vocals",
-    "Keyboards",
-    "Saxophone",
-    "Trumpet",
-    "Trombone",
-  ];
-
+const seedDatabase = async () => {
   const createInstruments = instruments.map(async (inst: string) => {
     return await prisma.instrument.create({
       data: {
@@ -23,9 +11,56 @@ const seedInstruments = async () => {
     });
   });
   await Promise.all(createInstruments);
-}
 
-seedInstruments()
+  const createMusicians = musicians.map(async (musician) => {
+    const { name, phoneNumber, email, instrument } = musician;
+    return await prisma.musician.create({
+      data: {
+        name,
+        phoneNumber,
+        email,
+        instruments: {
+          create: [
+            {
+              instrument: {
+                connectOrCreate: {
+                  where: {
+                    name: instrument,
+                  },
+                  create: {
+                    name: instrument,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+  });
+  await Promise.all(createMusicians);
+
+  const createVenues = venues.map(async (venue) => {
+    const { name, location } = venue;
+    const { address, city, zipCode, state } = location;
+    return await prisma.venue.create({
+      data: {
+        name,
+        location: {
+          create: {
+            address,
+            city,
+            zipCode,
+            state,
+          },
+        },
+      },
+    });
+  });
+  await Promise.all(createVenues);
+};
+
+seedDatabase()
   .then(async () => {
     await prisma.$disconnect();
   })
