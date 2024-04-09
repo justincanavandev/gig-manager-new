198ceb612b43bf604d153cf47da0d740926c3fcd
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-08 20:40:46 -0500

    deleted post boiler plate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { CreatePost } from "~/app/_components/create-post";
+// import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 
 export default async function Home() {
 
@@ -39,17 +39,17 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest();
+  // const latestPost = await api.post.getLatest();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
       )}
 
-      <CreatePost />
+      <CreatePost /> */}
     </div>
   );
 }
