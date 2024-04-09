66327de395ddc6ad4f5b13c3f1cbbd2041d4232d
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-09 08:18:21 -0500

    added seed command to seed gigs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import { musicians, instruments, venues } from "./seedData";
+import { musicians, instruments, venues, gigs } from "./seedData";
 
 const seedDatabase = async () => {
   const createInstruments = instruments.map(async (inst: string) => {
@@ -58,6 +58,54 @@ const seedDatabase = async () => {
     });
   });
   await Promise.all(createVenues);
+
+  const createGigs = gigs.map(async (gig) => {
+    const { name, startTime, endTime, venue, musicians, instrumentation } = gig;
+
+    const musicianObjs = await prisma.musician.findMany({
+      select: {
+        name: true,
+        id: true,
+      },
+    });
+
+    const musicianIds = musicians.map((m)=> {
+      const match = musicianObjs.find((mus)=> m === mus.name)
+      return match
+    }).map((ms)=> ms?.id)
+
+    return await prisma.gig.create({
+      data: {
+        name,
+        startTime,
+        endTime,
+        venue: {
+          connect: {
+            name: venue,
+          },
+        },
+        musicians: {
+          create: musicianIds.map((id) => ({
+            musician: {
+              connect: {
+                id,
+              },
+            },
+          })),
+        },
+        instrumentation: {
+          create: instrumentation.map((name) => ({
+            instrument: {
+              connect: {
+                name,
+              },
+            },
+          })),
+        },
+      },
+    });
+  });
+  await Promise.all(createGigs);
 };
 
 seedDatabase()
