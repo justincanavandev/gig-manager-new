e2229b3c26099f6dac6f6de4ebe784c881e2a76f
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-10 11:49:35 -0500

    changed data fetching to be done on server for get all gigs

--- src/app/_components/gigs/GigCard.tsx
+++ src/app/_components/gigs/GigCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { parseDate } from "~/server/utils/helpers";
 import { useState } from "react";
 import { useBoolean } from "usehooks-ts";
@@ -41,34 +43,30 @@ const GigCard = ({ gig, index }: GigCardProps) => {
   };
 
   return (
-    <>
-      {gig && (
-        <div className="flex flex-col" key={gig.id}>
-          <h2>{gig.name}</h2>
-          <span>Start Time: {parseDate(gig.startTime).parsedDate}</span>
-          <span>End Time: {parseDate(gig.endTime).parsedDate}</span>
-          {gig.venue && <span>Location: {gig.venue?.name}</span>}
-          <button
-            onClick={() => showHideMusicians(index)}
-            className="w-[8rem] border"
-          >
-            {viewMusicians && eventTargetIndexArr?.includes(index)
-              ? "Hide Musician List"
-              : "View Musician List"}
-          </button>
-          {viewMusicians && eventTargetIndexArr?.includes(index) && (
-            <div className="">
-              {musicians.map((musician) => (
-                <div key={musician.musicianId} className="flex">
-                  <p>{musician.musician.name}</p>
-                  <p>{musician.musician.instruments[0]?.instrument.name}</p>
-                </div>
-              ))}
+    <div className="flex flex-col" key={gig.id}>
+      <h2>{gig.name}</h2>
+      <span>Start Time: {parseDate(gig.startTime).parsedDate}</span>
+      <span>End Time: {parseDate(gig.endTime).parsedDate}</span>
+      {gig.venue && <span>Location: {gig.venue?.name}</span>}
+      <button
+        onClick={() => showHideMusicians(index)}
+        className="w-[8rem] border"
+      >
+        {viewMusicians && eventTargetIndexArr?.includes(index)
+          ? "Hide Musician List"
+          : "View Musician List"}
+      </button>
+      {viewMusicians && eventTargetIndexArr?.includes(index) && (
+        <div className="">
+          {musicians.map((musician) => (
+            <div key={musician.musicianId} className="flex">
+              <p>{musician.musician.name}</p>
+              <p>{musician.musician.instruments[0]?.instrument.name}</p>
             </div>
-          )}
+          ))}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -1,28 +1,14 @@
-'use client'
-
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
 import GigCard from "../_components/gigs/GigCard";
 
-const Gigs = () => {
-  const {
-    data: gigData,
-    isLoading: isGigLoading,
-
-  } = api.gig.getAll.useQuery();
+const Gigs = async () => {
+  const gigData = await api.gig.getAll();
 
   return (
     <div className="flex flex-col gap-8">
-      {isGigLoading ? (
-        <div>Loading...</div>
-      ) : (
-        gigData?.map((gig, index) => (
-          <>
-            <div>
-              <GigCard key={gig.id} gig={gig} index={index} />
-            </div>
-          </>
-        ))
-      )}
+      {gigData?.map((gig, index) => (
+        <GigCard key={gig.id} gig={gig} index={index} />
+      ))}
     </div>
   );
 };
