fc0d3c4cb2a66ea0fbe2f3cdea101b25b62759d8
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-16 19:48:31 -0500

    added startTime and endTime for gigs

--- src/app/_components/gigs/GigCard.tsx
+++ src/app/_components/gigs/GigCard.tsx
@@ -1,5 +1,5 @@
 import { parseDate } from "~/server/utils/helpers";
-// import MusiciansModal from "./MusiciansModal";
+import MusiciansModal from "./MusiciansModal";
 import type { SingleGig } from "~/server/types/gigTypes";
 
 type GigCardProps = {
@@ -8,16 +8,19 @@ type GigCardProps = {
 };
 
 const GigCard = ({ gig, index }: GigCardProps) => {
-
-  // const { musicians } = gig;
+  const { musicians } = gig;
+  const { parsedDate: startDate, parsedTime: startTime } = parseDate(
+    gig.startTime,
+  );
+  const { parsedDate: endDate, parsedTime: endTime } = parseDate(gig.endTime);
 
   return (
     <div className="flex flex-col" key={gig.id}>
       <h2>{gig.name}</h2>
-      <span>Start Time: {parseDate(gig.startTime).parsedDate}</span>
-      <span>End Time: {parseDate(gig.endTime).parsedDate}</span>
-      {/* <span>Location: {gig.venue ? gig.venue.name : "TBD"}</span>
-      <MusiciansModal musicians={musicians} index={index}/> */}
+      <span>{`Start Time: ${startDate}, ${startTime}`}</span>
+      <span>{`End Time: ${endDate}, ${endTime}`}</span>
+      <span>Location: {gig.venue ? gig.venue.name : "TBD"}</span>
+      <MusiciansModal musicians={musicians} index={index} />
     </div>
   );
 };
