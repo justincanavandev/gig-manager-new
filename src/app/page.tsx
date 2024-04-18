aabecf4d1e3a4d8b7a159b3f056f5911ed52bc26
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-18 14:18:51 -0500

    deleted commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
-// import { api } from "~/trpc/server";
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  // const userById = await api.user.getById({
-  //   id: session?.user ? session.user.id : "",
-  // });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
