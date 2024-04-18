5dfb217c2976069f15d08f468a2ebf82b4e50fff
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-18 08:31:49 -0500

    set up deleting instrument from gigForm. These will delete all musicians who play that instrument on the gig as well

--- src/app/_components/gigs/create/GigForm.tsx
+++ src/app/_components/gigs/create/GigForm.tsx
@@ -8,7 +8,7 @@ import MusicianSelector from "./MusicianSelector";
 import { useDispatch } from "react-redux";
 import { setGigForm, useGigForm } from "~/lib/features/gig/gigSlice";
 import { api } from "~/trpc/react";
-import { useEffect, type ChangeEvent } from "react";
+import { useEffect, type ChangeEvent, useState } from "react";
 import type { GigById } from "~/server/types/gigTypes";
 
 type GigFormProps = {
@@ -18,6 +18,14 @@ type GigFormProps = {
 const GigForm = ({ gig }: GigFormProps) => {
   const dispatch = useDispatch();
   const gigForm = useGigForm();
+  const [instName, setInstName] = useState<string>("");
+
+  const { refetch: getInstByName } = api.instrument.getByName.useQuery(
+    { name: instName },
+    {
+      enabled: !!instName,
+    },
+  );
 
   useEffect(() => {
     if (gig) {
@@ -76,6 +84,34 @@ const GigForm = ({ gig }: GigFormProps) => {
     );
   };
 
+  const deleteInstrument = async (inst: string) => {
+
+    setInstName(inst);
+    const instByName = await getInstByName();
+
+    const musicianIdsForInst = instByName.data?.musicians.map(
+      (mus) => mus.musicianId,
+    );
+
+    const { instrumentation, musicianIds } = gigForm;
+
+    const filteredMusicianIds = musicianIds.filter((id) =>
+      !musicianIdsForInst?.includes(id),
+    );
+
+    const filteredInsts = instrumentation.filter(
+      (instrument) => instrument !== inst,
+    );
+
+    dispatch(
+      setGigForm({
+        ...gigForm,
+        instrumentation: filteredInsts,
+        musicianIds: filteredMusicianIds,
+      }),
+    );
+  };
+
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -98,7 +134,16 @@ const GigForm = ({ gig }: GigFormProps) => {
         <DateSelector />
         <InstrumentSelector action={selectInstrument} nameOrId="name" />
         <VenueSelector />
+        <ul className="flex gap-2">
+          {gigForm.instrumentation.map((inst, index) => (
+            <>
+              <li key={`gigForm-${inst}-${index}`}>{inst}</li>
+              <span onClick={() => deleteInstrument(inst)}>x</span>
+            </>
+          ))}
+        </ul>
         <MusicianSelector />
+
         <button className="w-24 border border-black" type="submit">
           Submit
         </button>
