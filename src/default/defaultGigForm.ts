f0dda2faa05c4745d532c5142bf21cdd57c04870
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-05-14 18:45:58 -0500

    fixed type error on GigForm startTime/end

--- src/default/defaultGigForm.ts
+++ src/default/defaultGigForm.ts
@@ -3,8 +3,8 @@ import type { GigForm } from "~/server/types/gigTypes";
 export const defaultGigForm: GigForm = {
   name: "",
   venue: null,
-  startTime: new Date().toISOString(),
-  endTime: new Date().toISOString(),
+  startTime: new Date(),
+  endTime: new Date(),
   instrumentation: [],
   musicians: [],
 };
