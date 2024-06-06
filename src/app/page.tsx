b5452a9c90f0b788d7431375646b6b3d005387f5
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-06-06 10:08:00 -0500

    deleted crud showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="bg-gradient-to-b flex min-h-screen flex-col items-center justify-center from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         {/* <BaseButton as="link" href={`/musicians`}>Musicians Page</BaseButton> */}
         {/* <BaseButton as="link" href={`/gigs`}>Gigs page</BaseButton> */}
-        <BaseButton as="link" href={`/gigs/create`}>Gig Create</BaseButton>
-        {session?.user && <BaseButton as="link" href={`/user/${session.user.id}`}>User page</BaseButton>}
+        <BaseButton as="link" href={`/gigs/create`}>
+          Gig Create
+        </BaseButton>
+        {session?.user && (
+          <BaseButton as="link" href={`/user/${session.user.id}`}>
+            User page
+          </BaseButton>
+        )}
         <div className="flex flex-col items-center gap-2">
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-white">
@@ -19,34 +25,12 @@ export default async function Home() {
             <BaseButton
               as="link"
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
-         
             >
               {session ? "Sign out" : "Sign in"}
             </BaseButton>
           </div>
         </div>
-
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  // const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {/* {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost /> */}
-    </div>
   );
 }
